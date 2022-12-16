fbb35c63154fc289cdfab7350ba4e3395871d5a4
Ryan Helgering <92016038+0-MONK-0@users.noreply.github.com>
2022-12-16 19:37:15 +0100

    Update V0.5
    Animations

---
+++ Assets/Scripts/Player Movement/Used/PlayerController.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+
+    private Rigidbody2D rb;
+
+    private bool facingRight = true;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    private int extraJump;
+    public int extraJumpValue;
+
+    public Animator Animator;
+
+    [Header("Events")]
+    [Space]
+
+    public UnityEvent OnLandEvent;
+
+    [System.Serializable]
+    public class BoolEvent : UnityEvent<bool> { }
+
+    public BoolEvent OnCrouchEvent;
+    private bool m_wasCrouching = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        extraJump = extraJumpValue;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        Animator.SetFloat("Speed", Mathf.Abs(moveInput));
+
+        moveInput = Input.GetAxis("Horizontal");
+        Debug.Log(moveInput);
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        if (facingRight == false && moveInput > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
+    }
+
+    public void OnLanding ()
+    {
+        Animator.SetBool("IsJumping", false);
+    }
+
+    void Update()
+    {
+        if (isGrounded == true)
+        {
+            extraJump = extraJumpValue;
+            OnLandEvent.Invoke();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Y) && extraJump > 0)
+        {
+            Animator.SetBool("IsJumping", true);
+            rb.velocity = Vector2.up * jumpForce;
+            extraJump--;
+        }
+        else if (Input.GetKeyDown(KeyCode.Y) && extraJump == 0 && isGrounded == true)
+        {
+            Animator.SetBool("IsJumping", true);
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+
+        transform.Rotate(0f, 180f, 0f);
+
+        
+    }
+}
